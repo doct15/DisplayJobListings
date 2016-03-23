6a38c01ca155b7ce563e95742073634c075112ee
Ches R. Denison <cdenison@cslico.com>
2016-03-22 22:29:50 -0400

    Please FOXING work this time please

--- TestProject/Display-Job-Listings/app.js
+++ TestProject/Display-Job-Listings/app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname, '/client'));
+app.use(express.static(__dirname + '/client'));
 //app.use(express.static('client'))
 //app.use('/', client);
 
